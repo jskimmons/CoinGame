dedea5c4de7d71c9daed8f6ce8f8db53fdb2e292
Joseph Skimmons <JoeSkimmons@dyn-160-39-217-166.dyn.columbia.edu>
2017-01-26 22:49:53 -0500

    Added game features

--- CoinGame.js
+++ CoinGame.js
@@ -11,8 +11,16 @@ var quarter;
 var dime;
 var nickel;
 var penny;
+var price;
+var priceText;
+var score;
+var scoreText;
+var coins;
+var pointer;
 
 function create(){
+
+	score=0;
 
 	//scaling
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -24,9 +32,23 @@ function create(){
 
    	// sprites for one coin and coin purse
     quarter = game.add.sprite(50, 50, 'quarter');
-   	dime = game.add.sprite(150, 50, 'dime');
+    quarter.initialX = 50;
+    quarter.value = 25;
+    quarter.events.onDragStop.add(onDragStop, this);
+   	dime = game.add.sprite(170, 50, 'dime');
+   	dime.initialX = 170;
+   	dime.value = 10;
+   	dime.events.onDragStop.add(onDragStop, this);
     nickel = game.add.sprite(250, 50, 'nickel');
+    nickel.initialX = 250;
+    nickel.value = 5;
+    nickel.events.onDragStop.add(onDragStop, this);
     penny = game.add.sprite(350, 50, 'penny');
+    penny.initialX = 350;
+    penny.value = 1;
+    penny.events.onDragStop.add(onDragStop, this);
+
+    coins = [quarter, dime, nickel, penny];
 
     //  Input Enable the sprites
     quarter.inputEnabled = true;
@@ -40,8 +62,42 @@ function create(){
     nickel.input.enableDrag(true);
     penny.input.enableDrag(true);
 
+    // Random price
+    price = randPrice();
+    // Add game text
+    priceText = game.add.text(550, 16, 'Price: ' + price + ' cents', { font: "32px Verdana", fill: '#fff' });
+    scoreText = game.add.text(528, 60, 'Added: ' + score + ' cents', { font: "32px Verdana", fill: '#fff' });
+
 }
 
 function update(){
+	if(score == price){
+		score = 0;
+		scoreText.text = 'You Win!';
+		price = randPrice();
+		priceText.text = 'Price: ' + price + ' cents'
+	}
+	if(score>price){
+		scoreText.text = 'You Lose';
+		restart();
+	}
+}
 
+function randPrice(){
+	return Math.ceil(Math.random() * (99 - 1) + 1);
+}
+
+function restart(){
+	score = 0;
+	scoreText.text = 'Score: ' + score;
+}
+
+
+function onDragStop(sprite, pointer){
+	if (sprite.position.y > 300) {
+			sprite.position.x=sprite.initialX;
+			sprite.position.y=50;
+			score += sprite.value;
+			scoreText.text = 'Score: ' + score;
+		}
 }
